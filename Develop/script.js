55b85cf95e8e60f03d76f93a92689630f28077b5
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-09-29 18:44:42 -0700

    final bug fixes, added a reset button and tested it

--- Develop/script.js
+++ Develop/script.js
@@ -2,66 +2,63 @@
 // array of numbers, letters (cap and small) and special characters
 var result = "";
 var charLength;
-var arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
+var arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var arrayOfLowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var arrayOfUppers = arrayOfLowers.map(function(x){ return x.toUpperCase();})
+var arrayOfUppers = arrayOfLowers.map(function (x) { return x.toUpperCase(); })
 
 var arrayOfSpecialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 var availableCharacters = [];
 
-//determine length of password
-//create empty array
-  //will have to populate array with a for loop (for array, x = 0, x <charLength, x++)
 function generatePassword() {
+
+  result = "";
+
   //conditionals that force user to enter a number w/in range
   var flag = true
-  while(flag) {
-    var input = prompt("How many characters would you like?"); 
-    // console.log(input); //charlength = what was filled into prompt
-
-    if (Number.isInteger(Number.parseInt(input)) !== true) { //charlength NaN
+  while (flag) {
+    var input = prompt("How many characters would you like?");
+    //charlength is not an integer
+    if (Number.isInteger(Number.parseInt(input)) !== true) { 
       window.alert('Invalid input - please enter a number');
       continue;
-
-    } else if (Number.parseInt(input) < 8 || Number.parseInt(input) > 128) { //charlength falls out of range
+      // charlength falls out of range
+    } else if (Number.parseInt(input) < 8 || Number.parseInt(input) > 128) { //
       window.alert('iInvalid input - please enter a number within range (8-128)');
       continue;
+    //charlength = what was filled into prompt
+    } else if (Number.parseInt(input) >= 8 && Number.parseInt(input) <= 128) { 
 
-    } else if (Number.parseInt(input) >= 8 && Number.parseInt(input) <= 128) { //charlength = what was filled into prompt
-            
       charLength = Number.parseInt(input);
-      flag = false;
+      flag = false; //exit loop
     }
   }
-  // console.log("my " + charLength);
   //use conirms to ask if they want numbers, uppercase, LC, SP
-    //store their answer
+  //store their answer
   var includeNumbers = confirm("Do you want to include numbers in your password?");
   var includeSpecials = confirm("Do you want to include special characters in your password?");
   var includeUppers = confirm("Do you want to include uppercase letters in your password?");
   var includeLowers = confirm("Do you want to include lowercase letters in your password?");
-  
+
   if (includeNumbers) {
     availableCharacters = availableCharacters.concat(arrayOfNumbers);
   }
-  
+
   if (includeSpecials) {
     availableCharacters = availableCharacters.concat(arrayOfSpecialChar);
   }
-  
+
   if (includeUppers) {
     availableCharacters = availableCharacters.concat(arrayOfUppers);
   }
-  
+
   if (includeLowers) {
     availableCharacters = availableCharacters.concat(arrayOfLowers);
   }
-  // console.log(availableCharacters);
 
-   //Edge case: All prompts are false
+  //Edge case: All prompts are false
   if ((includeNumbers !== true) && (includeSpecials !== true) && (includeUppers !== true) && (includeLowers !== true)) {
     result = "";
     generatePassword();
@@ -69,28 +66,18 @@ function generatePassword() {
   }
   // at this point, we should have an array of concatted arrays for confirms answered yes
   //loop over available characters (for (i = 0, i < carLength, i++))
-    //need to use math.floor items[Math.floor(Math.random()*items.length)] - not necessarily right
-  for (var i = 0; i < charLength; i++ ) {
+  //need to use math.floor items[Math.floor(Math.random()*items.length)]
+  for (var i = 0; i < charLength; i++) {
 
-    result = result.concat(availableCharacters[Math.floor(Math.random()*availableCharacters.length)]); //this randomizes the array items selected
-  //as we loop, add character to currently empty string result
-  // console.log(result);
+    result = result.concat(availableCharacters[Math.floor(Math.random() * availableCharacters.length)]); //this randomizes the array items selected
+    //as we loop, add character to currently empty string result
+    // console.log(result);
   }
-  //only thing is that we need to make sure it gets at least one of every character type that is confirmed
-  //if they didnt say yes to any prompt, they have to be directed back to start?
-  
-  console.log(result); //just giving me the first 15 of the array
-  //need to randomize AND make sure I get at least one of each
-
- 
-  //TODO: Edge case: What if generated password does not contain characters from every true prompt
-
-  
-  
-    return result;
+  return result;
 }
 
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -98,13 +85,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
-  // var generateAgain = confirm("Would you like to generate another password?");
 
-  // if (generateAgain) {
-  //   generatePassword();
-  // }
+}
+generateBtn.addEventListener("click", writePassword); // when button is clicked
+
+var resetBtn = document.querySelector("#reset");
+
+function resetPassword() {
+  
+  var passwordReset = "";
+  var passwordResetText = document.querySelector("#password");
+  passwordResetText.value = passwordReset;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); // when button is clicked
+resetBtn.addEventListener("click", resetPassword);
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Random-Password-Generator
+Description:
+
+This repo contains a random password generator, which I programmed the JavaScript functionality into.
+
+If the user clicks the "generate password" button, they are prompted to enter a desired password length. If they enter anything other than a number between 8 and 128, they are alerted with correction criteria to force them to select a proper input.
+
+The user is then given a series of confirmations to decide what character types (uppercase, lowercase, number or special characters) they would like their password.
+
+Once all the information is collected, the generator displays a password on screen.
+
+The user then has the option to click the "reset password" button, which will then restart the password generator.
+
+Installation: N/A
+
+Usage: N/A
+
+Credits: Nathan Alexander
+
+License: Please refer to the license in the repo
+
+Badges: N/A
+
+Features: Function buttons to generate and reset password. Options for character types and length designation of password.
+
+How to Contribute: N/A
+
+Tests: N/A
+
+Screenshot:
+
+![Screenshot-of-password-generator](./Assets/password-generator-screenshot.png)
+
+Deployed Link:
